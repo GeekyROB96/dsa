60e8c9e4413d08467489e36cc484b2ed3b74fe2e
GeekyROB96 <rohitrk9693@gmail.com>
2023-11-27 18:02:35 +0530

    tries insetion

--- tries_dsaa/tries_intro.java
+++ tries_dsaa/tries_intro.java
@@ -18,5 +18,28 @@ class Solution{
     }
     public static Node root = new Node();   
 
+    public static void insert(String word){
+
+        Node curr = root;
+        for(int level =0;level<word.length();level++){
+            int idx = word.charAt(level)- 'a';
+            if(curr == null){
+                curr.children[idx] = new Node();
+
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
    
+
+    public static void main(String[] args) {
+        String words[]  ={"the","a","there","any","thee"};
+
+        for(int i =0;i<words.length;i++){
+            insert(words[i]);
+
+        }
+        }
 }
