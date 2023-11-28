767482dedf5384a4e21f039944d9ae752703eb42
GeekyROB96 <rohitrk9693@gmail.com>
2023-11-28 18:49:37 +0530

    added comments in tries_intro

--- tries_dsaa/tries_intro.java
+++ tries_dsaa/tries_intro.java
@@ -4,56 +4,54 @@ import java.util.*;
 
 class Solution{
 
-    //creation
+    // Trie Node definition
     static class Node{
         Node children[] = new Node[26];
-        boolean eow = false; //end of word check
+        boolean eow = false; // end of word check
     
-     Node(){
-        for(int i =0;i<26;i++){
-            children[i] = null;
+        Node(){
+            for(int i = 0; i < 26; i++){
+                children[i] = null;
+            }
         }
     }
     
-    }
+    // Trie root initialization
     public static Node root = new Node();   
 
-    public static void insert(String word){  ///Tc - O(l)
-
+    // Insert operation in Trie
+    public static void insert(String word){  /// Tc - O(l)
         Node curr = root;
-        for(int level =0;level<word.length();level++){
-            int idx = word.charAt(level)- 'a';
-            if(curr == null){
+        for(int level = 0; level < word.length(); level++){
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null){
                 curr.children[idx] = new Node();
-
             }
             curr = curr.children[idx];
         }
-
-        curr.eow = true;
+        curr.eow = true; // Marking end of word
     }
    
-
+    // Search operation in Trie
     public static boolean search(String key){
         Node curr = root;
-        for(int level =0;level<key.length();level++){
+        for(int level = 0; level < key.length(); level++){
             int idx = key.charAt(level) - 'a';
-
-            if(curr == null){
+            if(curr == null || curr.children[idx] == null){
                 return false;
             }
             curr = curr.children[idx];
         }
-
-        return curr.eow == true;
+        return curr.eow; // Check if it is the end of a word
     } 
 
+    // Main function for testing
     public static void main(String[] args) {
-        String words[]  ={"the","a","there","any","thee"};
+        String words[]  = {"the", "a", "there", "any", "thee"};
 
-        for(int i =0;i<words.length;i++){
+        // Inserting words into the Trie
+        for(int i = 0; i < words.length; i++){
             insert(words[i]);
-
         }
-        }
+    }
 }
